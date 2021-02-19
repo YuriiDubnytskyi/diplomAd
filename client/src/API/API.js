f908c804e50ed56e0e031edd3d32fcc03be37265
Yurii Dubnytskyi <41127730+YuriiDubnytskyi@users.noreply.github.com>
2021-02-19 18:20:38 +0200

    Update API.js

--- client/src/API/API.js
+++ client/src/API/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: `${process.env.REACT_APP_SERVER_API}/api/`,
+    //baseURL: `${process.env.REACT_APP_SERVER_API}/api/`,
+    baseURL: `/api/`,
     responseType: "json",
 });
